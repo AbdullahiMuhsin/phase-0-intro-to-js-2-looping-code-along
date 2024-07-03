9aafb3540abba6498bcdbf42c505a7efc753ef6f
abdullahi abdi <abdullahimuxsin398@gmail.com>
2024-07-03 18:11:26 +0300

    Loop lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(namesArray, surprise){
+    let cards = [];
+    for (let i = 0; i < namesArray.length; i++){
+        let messages = `Thank you, ${namesArray[i]}, for the wonderful ${surprise} gift!`
+        cards.push(messages);
+    }
+    return cards;
+}
+function countDown(start){
+for(let j = start; j >= 0; j--){
+    console.log(j);
+}
+}
+
+
+
